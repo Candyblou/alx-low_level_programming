58ea987c7a5ddc077021e0d12fab2e86f0185b4d
Candyblou <maphudufudu@gmail.com>
2023-10-18 13:55:34 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,21 +7,23 @@
 int main(void)
 {
 	char c;
-
 	char d;
 
 	c = 'a';
 	d = 'A';
+
 		while (c <= 'z') 
 		{
 				putchar(c);
 					c++;
 			}
-	while (d <= 'z') 
+	while (d <= 'Z') 
 	{
 			putchar(d);
 			d++;
 		}
+	
 	putchar('\n');
+
 	return (0);
 }
