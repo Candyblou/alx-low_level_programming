3bd5076f9bc104688b82ad755c7d44f73c71def2
Candyblou <maphudufudu@gmail.com>
2023-10-18 07:26:48 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-#include 
+#include <stdio.h>
+/**
+ * main - Program prints using printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
