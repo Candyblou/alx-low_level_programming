950d7866479f547de2eb723515f1ea64ef188061
Candyblou <maphudufudu@gmail.com>
2023-10-19 03:12:55 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,12 @@
 /**
  * _isalpha - Check if a character is an alphabetic character (a-z or A-Z).
  * @c: The character to check
- * Return 1 for alphabetic character or 0 for other
+ * Return: 1 for alphabetic character or 0 for other
  */
 
 int _isalpha(int c)
 
 {
-	
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 	return (1);
