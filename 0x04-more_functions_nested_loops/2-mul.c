bc32b5ab712d5973dda084a644c65c4bcede2e78
Candyblou <maphudufudu@gmail.com>
2023-10-20 04:22:46 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - Multiplies 2 integers
+ * @x: First nm multiplied
+ * @y: second nr being mulriplied by first nr
+ * Return: sum output
+ */
+
+int mul(int x, int y)
+
+{
+	return (x * y);
+}
