5d7d5d6680e2876251392d102615eab34d3f121c
Candyblou <maphudufudu@gmail.com>
2023-10-20 03:50:07 -0700

    0-isuper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _isupper - checks uppercase letters
+* @x: The number that needs to be to be checked
+* Return: 1 for uppercase, 0 for lowercase
+*/
+
+int _isupper(int x)
+
+{
+if (x >= 65 && x <= 90)
+{
+return (1);
+}
+return (0);
+}
