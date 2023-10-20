a68a58abf36300e3ddf2baa974f1cf554caf6b93
Candyblou <maphudufudu@gmail.com>
2023-10-20 11:10:08 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _putchar - writes out character
+ * @c: character written
+ * print_numbers - prints nrs from 0 to 9
+ * Return: Nrs from 0 to 9
+ */
+
+int _putchar(char c);
+
+void print_numbers(void)
+
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+	_putchar(x + '0');
+	}
+	_putchar('\n');
+}
